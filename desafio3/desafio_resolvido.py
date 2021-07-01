0ff9eab6a2a236091fae5fab7908d779d6199c72
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2021-07-01 16:32:03 -0300

    Segunda parte do desafio implementada

--- desafio3/desafio_resolvido.py
+++ desafio3/desafio_resolvido.py
@@ -1,6 +1,24 @@
 nome = input("Qual o seu nome: ")
-altura = float(input("Digite sua altura em metros: "))
-peso = float(input("Digite seu peso em Kg: "))
+altura = 0.0
+peso = 0.0
+
+while (altura < 0.1 or altura > 2.4) :
+    altura = float(input("Digite sua altura em metros: ").replace(",", "."))
+    if (altura > 2.4) :
+        print("Altura inválida. Não existe uma pessoal tão alta assim!")
+    elif(altura < 0.1) :
+        print("Altura inválida. Não existe uma pessoa tão baixa assim!")
+    else :
+        break
+
+while (peso < 2.1 or peso > 500) :
+    peso = float(input("Digite seu peso em Kg: ").replace(",", "."))
+    if (peso < 2.1) :
+        print("Peso inválido. Não existe uma pessoal tão leve assim!")
+    elif(peso > 500) :
+        print("Peso inválido. Não existe uma pessoa tão pesada assim!")
+    else :
+        break
 
 imc = peso / altura ** 2
 
